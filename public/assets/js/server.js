7c7459da8a03b335198ec9abd4e7711335b14e54
jcerb <brt.jst@gmail.com>
2021-03-24 14:51:17 -0700

    start route code

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -3,6 +3,24 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static("public"));
+
+// HTML Routes
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+// API Routes
+app.get("/api/notes", function (req,res){
+    // retrieve all notes and res.json then back to the front end
+});
+app.post("/api/notes", function (req,res){
+    // creates a note from req.body
+});
+app.delete("/api/notes/:id", function (req, res){
+    // delete a note based off id
+})
 
 app.listen(PORT, () => console.log("App listening on PORT " + PORT));
