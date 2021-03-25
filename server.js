1d433795a237e466a379b048b00b37cdb46f8990
jcerb <brt.jst@gmail.com>
2021-03-25 09:59:05 -0700

    add to routes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const uuid = require("uuid")
+const { v4: uuidv4 } = require('uuid');
 const path = require("path");
 const http = require("http");
+const fs = require ("fs");
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// body parsing middleware?
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
@@ -21,14 +24,30 @@ app.get("/", function (req, res) {
 
 // API Routes
 app.get("/api/notes", function (req,res){
+    fs.promises.readFile("./db/db.json", "utf8", function(err,data) {
+        res.json(JSON.parse(data));
+    });
     // retrieve all notes and res.json then back to the front end
 });
 app.post("/api/notes", function (req,res){
-    const getNotes = req.body;
-    // creates a note from req.body
+    // creates a note from req.body apply uuid here?
+     const note = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text
+    };
+    fs.promises.readFile("./db/db.json", "utf8", function(err,data) {
+        res.json(JSON.parse(data));
+    });
+    // read the data from db.json
+    // ?parse out to the Arraystringify the array
+    // write to file with the new array
+    // respond to the user
+    
 });
 app.delete("/api/notes/:id", function (req, res){
     // delete a note based off id
+    const {id} = req.params;
 })
 
 app.listen(PORT, () => console.log("App listening on PORT " + PORT));
