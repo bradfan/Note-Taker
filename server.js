f0d67ef3f2db8216e450fcff0b1f5c47c1b98620
jcerb <brt.jst@gmail.com>
2021-03-24 18:13:01 -0700

    run server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = required("express");
-const app = express();
+const express = require("express");
+const uuid = require("uuid")
+const path = require("path");
+const http = require("http");
 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
@@ -8,15 +11,20 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // HTML Routes
-app.get("/notes", function (req, res){
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-});
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  app.get("/notes", function (req, res) {
+      res.sendFile(path.join(__dirname, "public", "notes.html"));
+    });
+  
 
 // API Routes
 app.get("/api/notes", function (req,res){
     // retrieve all notes and res.json then back to the front end
 });
 app.post("/api/notes", function (req,res){
+    const getNotes = req.body;
     // creates a note from req.body
 });
 app.delete("/api/notes/:id", function (req, res){
