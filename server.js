03883e7594604297f522e2a00e824a61002e4264
jcerb <brt.jst@gmail.com>
2021-03-26 10:00:31 -0700

    complete post

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const path = require("path");
 const http = require("http");
 const fs = require ("fs");
+const { json } = require("express");
 
 
 const app = express();
@@ -24,47 +25,41 @@ app.get("/", function (req, res) {
 
 // API Routes/lines 28-34 index.js
 app.get("/api/notes", function (req,res){
-    fs.promises.readFile("./db/db.json", "utf8", function(err,data) {
+    fs.readFile("./db/db.json", "utf8", function(err,data) {
         return err ? console.log(err) : res.json(JSON.parse(data));
     });
     // retrieve all notes and res.json then back to the front end
  }); 
 //  lines 36-43 index.js
 app.post("/api/notes", function (req,res){
-    fs.readFile("./db/db.json", "utf-8", function (err){
+    const note = {
+        id: uuidv4(), 
+        title: req.body.title,
+        text: req.body.text
+     };
+     console.log("note: ", note)
+     fs.readFile("./db/db.json", "utf-8", function (err, dataRead){
         if(err) throw err
         // read the data from db.json
         // creates a post from req.body 
-           const data = req.body
-        const note = {
-                id: uuidv4(),
-                title: req.body.title,
-                text: req.body.text
-             };
-             console.log("note: ", note)
-            
-            
-    // parse out to stringify the array
-        fs.writeFile("./db/db.json", JSON.stringify(note), function (err){
+        //    const data-note = req.body
+       console.log("dataREadString", dataRead)
+       // convert json string - parse
+       dataRead = JSON.parse(dataRead)
+       console.log("dataREadParsed", dataRead)
+        dataRead.push(note);
+        // stringify the array
+        fs.writeFile("./db/db.json", JSON.stringify(dataRead), function (err){
             if(err) throw err
-            res.sendFile(path.join(__dirname, "public", "notes.html"));
-            
+            res.json("/api/notes");
          }); 
     });  
-    
-    
-    
-    
-
-  
-    
-});
+    });
 // lines 45-51 index.js
 app.delete("/api/notes/:id", function (req, res){
-    fs.readFile("./db/db.json", "utf8", function (err,data){
+    fs.readFile("./db/db.json", "utf-8", function (err,data){
         res.json(JSON.parse(data));
     })
-
     // delete a note based off id
     const {id} = req.params;
 })
